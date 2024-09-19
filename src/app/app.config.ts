0bbf2c372b3ab4dbef73c761323e2fe4960f27da
Angel Jimenez <angel.jimenez@luminisoft.com>
2024-09-18 19:21:53 -0600

    *Se agrega uso de variables de entorno

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,8 @@ import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"fir-test-9e5e0","appId":"1:669980881535:web:6d4b21ee3df95afd39c54a","storageBucket":"fir-test-9e5e0.appspot.com","apiKey":"AIzaSyBgfpYuApcUgoINzyBUMPa3EOB0omUr-tA","authDomain":"fir-test-9e5e0.firebaseapp.com","messagingSenderId":"669980881535"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp(environment.firebase)), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    //Colocar datos de firebase
+    firebase: { }
+};
